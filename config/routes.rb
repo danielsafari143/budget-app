8e84d7150a9cff360c8b0dc7a4724f2c6f56b9e4
danielsafari143 <danielsafari143@gmail.com>
2023-09-11 09:01:14 +0200

    ADD ROUTES

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'splash#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  references :groupe do
+    references :entity
+  end
 end
