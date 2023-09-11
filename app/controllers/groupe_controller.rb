13f059d0c70b959cb323fe9fa74d82af8769ffe8
danielsafari143 <danielsafari143@gmail.com>
2023-09-11 18:26:18 +0200

    ADD CHANGES

--- app/controllers/groupe_controller.rb
+++ app/controllers/groupe_controller.rb
@@ -1,11 +1,17 @@
 class GroupeController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    def index
-      @groupe = current_user.groupes.includes(:entities).all
-    end
+  def index
+    @groupe = current_user.groupes.includes(:entities).all
+  end
 
-    def new
-      
-    end
+  def new
+    @groupe = current_user.groupes
+  end
+
+  def create
+    groupe = params[:Groupe]
+    Groupe.create(name: groupe['name'], icon: groupe['icon'], user: current_user)
+    redirect_to '/groupe'
+  end
 end
