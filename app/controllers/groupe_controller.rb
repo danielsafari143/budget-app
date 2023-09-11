2082a018789d29e3c09cc94604edda2d8938efdc
danielsafari143 <danielsafari143@gmail.com>
2023-09-11 17:31:44 +0200

    ADD CHANGES

--- app/controllers/groupe_controller.rb
+++ app/controllers/groupe_controller.rb
@@ -2,7 +2,10 @@ class GroupeController < ApplicationController
     before_action :authenticate_user!
 
     def index
-      @groupe = current_user.groupes.all
+      @groupe = current_user.groupes.includes(:entities).all
     end
 
+    def new
+      
+    end
 end
