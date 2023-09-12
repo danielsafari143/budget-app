5dc683beb2eb68aea97557eb873e216457b5f314
danielsafari143 <danielsafari143@gmail.com>
2023-09-12 21:13:58 +0200

    UPDATE README

--- spec/models/groupe_entity_spec.rb
+++ spec/models/groupe_entity_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe GroupeEntity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.create(name: 'Dan', email: 'dan@gmail.com', password: 'password123')
+    sign_in @user
+    @groupe = Groupe.create(name: 'Cinema', icon: 'https://www.thestreet.com', user: @user)
+    @entity = Entity.create(name: 'First transaction', amount: 345, user: @user)
+    @groupe.entities << @entity
+  end
+
+  it 'has a user name of "Dan"' do
+    expect(@groupe.entities.first.user.name).to eq('Dan')
+  end
+
+  it 'has a description of "First transaction"' do
+    expect(@groupe.entities.first.name).to eq('First transaction')
+  end
+
+  it 'has an amount of 345' do
+    expect(@groupe.entities.first.amount).to eq(345)
+  end
 end
--- spec/requests/splash_spec.rb
+++ spec/requests/splash_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Splashes', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET #index' do
+    it 'returns a successful response' do
+      get '/'
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the index template' do
+      get '/groupe/groupe'
+      expect(response).to have_http_status(302)
+    end
   end
 end
