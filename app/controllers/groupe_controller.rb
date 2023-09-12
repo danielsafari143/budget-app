27889c9bb9ca261c2948608f758ff07cd3bd8d39
danielsafari143 <danielsafari143@gmail.com>
2023-09-12 18:02:37 +0200

    FIX ERRORS

--- app/controllers/groupe_controller.rb
+++ app/controllers/groupe_controller.rb
@@ -9,6 +9,11 @@ class GroupeController < ApplicationController
     @groupe = current_user.groupes
   end
 
+  def show
+    @groupe = current_user.groupes.includes(:entities).find_by(id: params['id'])
+    @entity = @groupe.entities.order('created_at DESC')
+  end
+
   def create
     groupe = params[:Groupe]
     Groupe.create(name: groupe['name'], icon: groupe['icon'], user: current_user)
